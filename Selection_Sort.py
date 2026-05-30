066612d04cff2754c2f9d9bb218409bf8215630d
andres12go <aguevarao@unal.edu.co>
2026-05-30 16:38:37 +0200

    Delete Selection_Sort.py

--- Selection_Sort.py
+++
@@ -1,9 +0,0 @@
-def selection_sort(a):
-    for i in range(len(a)):
-        for j in range(i, len(a) - 1):
-            if a[i] > a[j+1]:
-                a[i], a[j+1] = a[j +1], a[i]
-
-    return a
-
-print(selection_sort([2, 4, 1]))
